1f92830e582cf977df6fe175ee57aff284281397
vaemc <vaemc520@qq.com>
2022-12-28 17:46:23 +0800

    立项

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div ref="editorContainer" style="height: 100vh; width: 100%"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import { SerialPort } from "serialport";
+import { ReadlineParser } from "@serialport/parser-readline";
 
+import * as monaco from "monaco-editor";
+SerialPort.list().then((ports) => {
+  let list = ports.map((item) => {
+    return { value: item.path, label: item.path };
+  });
+  console.info(list);
+});
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data(){
+    return{
+      monacoEditor: { }
+    }
+  },
+  mounted() {
+    this.monacoEditor = monaco.editor.create(this.$refs.editorContainer, {
+      value: "内容",
+      readOnly: true,
+      language: "java",
+      theme: "vs-dark",
+    });
+
+    this.monacoEditor.setValue("aaaaaaa");
+  },
+};
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  configureWebpack: {
+    externals: {
+      serialport: 'require("serialport")',
+    },
+  },
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+    },
+  },
+};
