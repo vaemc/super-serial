e12630453d83ca58af736e2fb6eb02bb56bf8f06
vaemc <vaemc520@qq.com>
2022-12-29 15:28:40 +0800

    1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,37 @@
             <div ref="editorContainer" style="height: 100vh; width: 100%"></div
           ></a-col>
           <a-col flex="200px">
-            <a-button type="primary"> Primary </a-button>
+            <a-form :label-col="{ span: 8 }" :wrapper-col="{ span: 12 }">
+              <a-form-item label="波特率" @change="baudRateSelectChange">
+                <a-select defaultValue="115200">
+                  <a-select-option value="9600">9600</a-select-option>
+                  <a-select-option value="74880">74880</a-select-option>
+                  <a-select-option value="115200">115200</a-select-option>
+                </a-select>
+              </a-form-item>
+              <a-form-item label="端口">
+                <a-select
+                  @focus="serialPortListSelectFocus"
+                  @change="serialPortListSelectChange"
+                >
+                  <a-select-option
+                    v-for="item in serial.portList"
+                    :key="item.value"
+                    :value="item.value"
+                  >
+                    {{ item.value }}
+                  </a-select-option>
+                </a-select>
+              </a-form-item>
+              <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
+                <a-button
+                  :type="serialPortOperationBtnType"
+                  @click="serialPortOperationBtn"
+                >
+                  {{ serialPortOperationBtnText }}
+                </a-button>
+              </a-form-item>
+            </a-form>
           </a-col>
         </a-row>
       </a-tab-pane>
@@ -29,36 +59,14 @@ require(["vs/editor/editor.main"], function () {
 </script> -->
 
 <script>
-
-
-
 import { SerialPort } from "serialport";
 import { ReadlineParser } from "@serialport/parser-readline";
 // import * as monaco from "monaco-editor";
-import loader from "@monaco-editor/loader";
+// import loader from "@monaco-editor/loader";
 import * as monaco from "monaco-editor";
-const path = require('path');
-
-
-function ensureFirstBackSlash(str) {
-    return str.length > 0 && str.charAt(0) !== '/'
-        ? '/' + str
-        : str;
-}
-
-function uriFromPath(_path) {
-    const pathName = path.resolve(_path).replace(/\\/g, '/');
-    return encodeURI('file://' + ensureFirstBackSlash(pathName));
-}
-
-console.info(__dirname)
-loader.config({
-  paths: {
-    vs: uriFromPath(
-      path.join(__dirname, '../node_modules/monaco-editor/min/vs')
-    )
-  }
-});
+// loader.config({
+//   monaco,
+// });
 
 // loader.config({
 //   "vs/nls": {
@@ -68,37 +76,114 @@ loader.config({
 //   },
 // });
 
-SerialPort.list().then((ports) => {
-  let list = ports.map((item) => {
-    return { value: item.path, label: item.path };
-  });
-  console.info(list);
-});
-
 export default {
   name: "HomeView",
   data() {
     return {
+      serialPortListSelectValue: "",
+      serialPortOperationBtnText: "打开端口",
+      serialPortOperationBtnType: "primary",
+
       monacoEditor: {},
+      serial: {
+        portList: [],
+        baudRate: "115200",
+        serial1: {},
+      },
     };
   },
-  mounted() {
-    loader.init().then((monaco) => {
-      monaco.editor.create(this.$refs.editorContainer, {
-        theme: "vs-dark",
-        readOnly: true,
-        value: "// some comment",
-        language: "javascript",
+  methods: {
+    baudRateSelectChange(value) {
+      this.serial.baudRate = value;
+    },
+    serialPortListSelectChange(value) {
+      console.info(value);
+      this.serialPortListSelectValue = value;
+    },
+    serialPortOperationBtn() {
+      if (this.serialPortListSelectValue === "") {
+        this.$message.warn("请选择端口");
+        return;
+      }
+
+      if (this.serialPortOperationBtnText == "打开端口") {
+        this.serial.serial1 = new SerialPort({
+          path: this.serialPortListSelectValue,
+          baudRate: parseInt(this.serial.baudRate),
+        });
+
+        const parser = this.serial.serial1.pipe(new ReadlineParser({ delimiter: "\r\n" }));
+        parser.on("data", (data) => {
+          console.info(data);
+
+          this.monacoEditor.getModel().applyEdits(
+            [
+              {
+                range: {
+                  startColumn: 0,
+                  startLineNumber:
+                    this.monacoEditor.getModel().getLineCount() + 1,
+                  endColumn: 0,
+                  endLineNumber:
+                    this.monacoEditor.getModel().getLineCount() + 1,
+                },
+                text: data + "\r\n",
+              },
+            ],
+            true
+          );
+
+          if (
+            this.monacoEditor.getPosition().lineNumber >=
+            this.monacoEditor.getModel().getLineCount()
+          ) {
+            this.monacoEditor.revealLine(
+              this.monacoEditor.getModel().getLineCount()
+            );
+          }
+        });
+
+        this.serialPortOperationBtnText = "关闭端口";
+        this.serialPortOperationBtnType = "danger";
+      } else if (this.serialPortOperationBtnText == "关闭端口") {
+        this.serialPortOperationBtnText = "打开端口";
+        this.serialPortOperationBtnType = "primary";
+
+        this.serial.serial1.port.close();
+      }
+    },
+    serialPortListSelectFocus() {
+      console.info("asd");
+      this.getSerialPortList();
+    },
+    getSerialPortList() {
+      SerialPort.list().then((ports) => {
+        let list = ports.map((item) => {
+          return { value: item.path, label: item.path };
+        });
+        this.serial.portList = list;
       });
-    });
+    },
+  },
+  mounted() {
+    this.getSerialPortList();
+
+    // loader.init().then((monaco) => {
+    //   this.monacoEditor = monaco.editor.create(this.$refs.editorContainer, {
+    //     theme: "vs-dark",
+    //     readOnly: true,
+    //     value: "// some comment",
+    //     language: "javascript",
+    //   });
+    // });
 
     // console.info(monaco.editor);
-    // this.monacoEditor = monaco.editor.create(this.$refs.editorContainer, {
-    //   value: "内容",
-    //   readOnly: true, //只读
-    //   language: "java",
-    //   theme: "vs-dark",
-    // });
+    this.monacoEditor = monaco.editor.create(this.$refs.editorContainer, {
+      value: "",
+      // readOnly: true, //只读
+      language: "java",
+      theme: "vs-dark",
+    });
 
     // this.monacoEditor.setValue("aaaaaaa");
   },
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 // module.exports = defineConfig({
 //   transpileDependencies: true
 // })
-
-
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 module.exports = {
+  chainWebpack: function (config) {
+    config.plugin("monaco").use(new MonacoWebpackPlugin());
+  },
   configureWebpack: {
     externals: {
       serialport: 'require("serialport")',
