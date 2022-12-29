333b3707a080b2f4985d6b4cc3fd86bae564e4f9
vaemc <vaemc520@qq.com>
2022-12-29 16:55:38 +0800

    1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # super_serial  开发中......
 
-使用Monaco Editor作为串口信息输出窗口
+使用Monaco Editor作为串口信息输出窗口，解决大多数串口工具搜索不友好的问题
 
 # 📔Todo...
 * 日志颜色解析ansi color 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,17 +4,44 @@
       <a-tab-pane key="1" tab="串口1">
         <a-row type="flex">
           <a-col flex="auto">
-            <div ref="editorContainer" style="height: 100vh; width: 100%"></div
-          ></a-col>
-          <a-col flex="200px">
+            <a-tabs tabPosition="right" default-active-key="1">
+              <a-tab-pane key="1" tab="文本">
+                <div
+                  ref="editorContainer"
+                  style="height: 72vh; width: 100%"
+                ></div>
+              </a-tab-pane>
+              <a-tab-pane key="2" tab="十六进制" force-render> </a-tab-pane>
+              <a-tab-pane key="3" tab="日志">
+                <div v-html="logHtml" style="white-space: pre"></div>
+              </a-tab-pane>
+            </a-tabs>
+
+            <a-row type="flex" align="middle" :gutter="16">
+              <a-col flex="auto">
+                <a-textarea
+                  style="margin: 5px"
+                  placeholder="请输入内容"
+                  :auto-size="{ minRows: 4, maxRows: 4 }"
+                  allow-clear
+              /></a-col>
+              <a-col flex="200px">
+                <div
+                  style="
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: flex-start;
+                    align-content: flex-start;
+                  "
+                >
+                  <a-checkbox> 16进制发送 </a-checkbox>
+                  <a-button type="primary"> 发送 </a-button>
+                </div>
+              </a-col>
+            </a-row>
+          </a-col>
+          <a-col flex="300px">
             <a-form :label-col="{ span: 8 }" :wrapper-col="{ span: 12 }">
-              <a-form-item label="波特率" @change="baudRateSelectChange">
-                <a-select defaultValue="115200">
-                  <a-select-option value="9600">9600</a-select-option>
-                  <a-select-option value="74880">74880</a-select-option>
-                  <a-select-option value="115200">115200</a-select-option>
-                </a-select>
-              </a-form-item>
               <a-form-item label="端口">
                 <a-select
                   @focus="serialPortListSelectFocus"
@@ -29,6 +56,14 @@
                   </a-select-option>
                 </a-select>
               </a-form-item>
+              <a-form-item label="波特率" @change="baudRateSelectChange">
+                <a-select defaultValue="115200">
+                  <a-select-option value="9600">9600</a-select-option>
+                  <a-select-option value="74880">74880</a-select-option>
+                  <a-select-option value="115200">115200</a-select-option>
+                </a-select>
+              </a-form-item>
+
               <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
                 <a-button
                   :type="serialPortOperationBtnType"
@@ -59,6 +94,8 @@ require(["vs/editor/editor.main"], function () {
 </script> -->
 
 <script>
+const Convert = require("ansi-to-html");
+const convert = new Convert();
 import { SerialPort } from "serialport";
 import { ReadlineParser } from "@serialport/parser-readline";
 // import * as monaco from "monaco-editor";
@@ -80,6 +117,7 @@ export default {
   name: "HomeView",
   data() {
     return {
+      logHtml: "",
       serialPortListSelectValue: "",
       serialPortOperationBtnText: "打开端口",
       serialPortOperationBtnType: "primary",
@@ -112,9 +150,13 @@ export default {
           baudRate: parseInt(this.serial.baudRate),
         });
 
-        const parser = this.serial.serial1.pipe(new ReadlineParser({ delimiter: "\r\n" }));
+        const parser = this.serial.serial1.pipe(
+          new ReadlineParser({ delimiter: "\r\n" })
+        );
         parser.on("data", (data) => {
           console.info(data);
+
+          this.logHtml += convert.toHtml(data)+ "\r\n";
 
           this.monacoEditor.getModel().applyEdits(
             [
@@ -153,13 +195,13 @@ export default {
       }
     },
     serialPortListSelectFocus() {
-      console.info("asd");
       this.getSerialPortList();
     },
     getSerialPortList() {
       SerialPort.list().then((ports) => {
         let list = ports.map((item) => {
-          return { value: item.path, label: item.path };
+          console.info(item);
+          return { value: item.path, label: item.friendlyName };
         });
         this.serial.portList = list;
       });
